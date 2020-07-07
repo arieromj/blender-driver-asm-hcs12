7f69c03ba2dde7b6ec955d9f1bb4a23ba86badb7
jonasandrade7 <67973278+jonasandrade7@users.noreply.github.com>
2020-07-07 16:08:54 -0300

    Add files via upload

--- p2_micro/Sources/main.asm
+++ p2_micro/Sources/main.asm
@@ -12,11 +12,39 @@
             XDEF Entry            ; export 'Entry' symbol
             ABSENTRY Entry        ; for absolute assembly: mark this as application entry point
 
+            INCLUDE MC9S12C128.inc
+
+
             ORG $1000
             
 Entry:
-                       
+            BSET DDRA,$04
+            BCLR DDRA,$03
+            BSET DDRB,$FF
+VERIFICA            
+            BRSET PORTB,$01,BOTAO1
+            BRSET PORTB,$02,BOTAO2
+            BRSET PORTB,$03,BOTAO3
+            BRSET PORTB,$04,BOTAO4            
+          
+            BRA VERIFICA
+            
+            
+BOTAO1:     
+         
+            RTS
+           
+BOTAO2:
 
+            RTS
+            
+BOTAO3: 
+
+            RTS
+            
+BOTAO4: 
+
+            RTS                                   
 
             ORG   $FFFE
             DC.W  Entry           ; Reset Vector
